24f0fee35180969806e20c8db97fa74b8dc77d63
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2025-02-11 12:36:43 +0100

    Add login and register pages to BlazorUI

---
+++ greenPomodoro.BlazorUI/Pages/Auth/Login.razor.cs
@@ -0,0 +1,39 @@
+﻿using greenPomodoro.BlazorUI.Contracts;
+using greenPomodoro.BlazorUI.Models.Auth;
+using Microsoft.AspNetCore.Components;
+
+namespace greenPomodoro.BlazorUI.Pages.Auth
+{
+    public partial class Login
+    {
+        public LoginVM Model { get; set; } = new();
+        private readonly IAuthService _authService;
+        private readonly NavigationManager _navigationManager;
+        public Login(IAuthService authService, NavigationManager navigationManager)
+        {
+            _authService = authService;
+            _navigationManager = navigationManager;
+        }
+
+        public async Task LoginUser()
+        {
+            var result = await _authService.Login(Model);
+            if(result)
+            {
+                _navigationManager.NavigateTo("/task");
+            }
+            // TODO: Add an error message for incorrect login credentials.
+        }
+
+        public async Task LoginDemo()
+        {
+            Model.Email = "admin@example.com";
+            Model.Password = "P@ssword2024";
+            var result = await _authService.Login(Model);
+            if (result)
+            {
+                _navigationManager.NavigateTo("/task");
+            }
+        }
+    }
+}
---
+++ greenPomodoro.BlazorUI/Pages/Auth/Register.razor.cs
@@ -0,0 +1,20 @@
+﻿using greenPomodoro.BlazorUI.Contracts;
+using greenPomodoro.BlazorUI.Models.Auth;
+
+namespace greenPomodoro.BlazorUI.Pages.Auth
+{
+    public partial class Register
+    {
+        public RegisterVM Model { get; set; } = new();
+        private readonly IAuthService _authService;
+        public Register(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task RegisterUser()
+        {
+            await _authService.Register(Model);
+        }
+    }
+}
